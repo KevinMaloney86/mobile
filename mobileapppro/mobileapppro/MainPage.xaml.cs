c3ec35a4d1d1e6976eff042c0c0ca9b4adfadf6c
KEVIN MALONEY - STUDENT <G00367608@gmit.ie>
2020-01-13 13:06:35 +0000

    first commit

---
+++ mobileapppro/mobileapppro/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace mobileapppro
+{
+    public partial class MainPage : ContentPage
+    {
+        int counter = 0;
+        Label l = new Label();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Countlabel();
+
+           
+
+        }
+        public void BtnCount_Clicked(object sender, EventArgs e)
+        {
+            ++counter;
+            string s = counter.ToString();
+            l.Text = s;
+            
+
+           
+        }
+
+        public void Countlabel()
+        {
+            l.Text = "0";
+            Clicker.Children.Add(l);
+        }
+    }
+}
